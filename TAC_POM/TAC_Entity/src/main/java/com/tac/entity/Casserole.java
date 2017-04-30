1bffc91b579fccf44740bf155c2333b8298fe611
1ud0 <ludoviccadet74@gmail.com>
2017-04-30 14:49:00 +0200

    coumi

--- TAC_POM/TAC_Entity/src/main/java/com/tac/entity/Casserole.java
+++ TAC_POM/TAC_Entity/src/main/java/com/tac/entity/Casserole.java
@@ -3,5 +3,11 @@ package com.tac.entity;
 public class Casserole {
 
 	public static void main(String[] args) {
+<<<<<<< Upstream, based on master
+=======
+		System.out.println("coucou");
+		System.out.println();
+		System.out.println();
+>>>>>>> 10924dc coumi
 	}
 }
