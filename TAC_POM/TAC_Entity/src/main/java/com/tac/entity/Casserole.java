08023baf23e034b8593c8c39a12cf74211713690
1ud0 <ludoviccadet74@gmail.com>
2017-04-30 14:51:47 +0200

    resolve conflict

--- TAC_POM/TAC_Entity/src/main/java/com/tac/entity/Casserole.java
+++ TAC_POM/TAC_Entity/src/main/java/com/tac/entity/Casserole.java
@@ -3,11 +3,6 @@ package com.tac.entity;
 public class Casserole {
 
 	public static void main(String[] args) {
-<<<<<<< Upstream, based on master
-=======
-		System.out.println("coucou");
-		System.out.println();
-		System.out.println();
->>>>>>> 10924dc coumi
+	
 	}
 }
